0fb5151525912cd5331b107a019d13d52e1998b4
Bus Logic Ltd <ticketing.rs@gmail.com>
2025-01-01 17:37:30 +0100

    Inicijalni commit

--- app/controllers/ScheduleController.php
+++ app/controllers/ScheduleController.php
@@ -21,14 +21,18 @@ class ScheduleController extends Controller {
     }
 
     public function store() {
+        // Preuzmi podatke iz POST zahteva
         $driver_id = $_POST['driver_id'];
         $bus_id = $_POST['bus_id'];
         $route_id = $_POST['route_id'];
         $departure_time = $_POST['departure_time'];
         $arrival_time = $_POST['arrival_time'];
 
+        // Kreiraj novi raspored
         $scheduleModel = $this->model('Schedule');
         $scheduleModel->create($driver_id, $bus_id, $route_id, $departure_time, $arrival_time);
+
+        // Preusmeri nazad na indeks stranicu rasporeda
         header('Location: /turnusi/public/schedule/index');
     }
 }
