9b8895567be2c6bda3f271b2669cf8fc2e7709be
Bus Logic Ltd <ticketing.rs@gmail.com>
2025-01-03 17:25:32 +0100

    Inicijalni commit

---
+++ app/controllers/TurnusController.php
@@ -0,0 +1,8 @@
+<?php
+// app/controllers/TurnusController.php
+
+class TurnusController {
+    public function index() {
+        require 'app/views/turnus/index.php';
+    }
+}
---
+++ app/views/turnusi/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Turnusi</title>
+</head>
+<body>
+    <h1>Turnusi</h1>
+    <p>Ovo je stranica za upravljanje turnusima.</p>
+</body>
+</html>
